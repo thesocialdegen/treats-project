f845a4f75460d9edc30ca81d77199c51c3c7cfca
thesocialdegen <thesocialdegen@gmail.com>
2024-06-28 20:54:49 +0530

    fix:added-campaigns-page

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -1,8 +1,20 @@
+import CampaignsPage from '@/components/Campaigns'
+import SEOHead from '@/global/SEOHead'
+import { bangers } from '@/utils/fonts'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>campaign page</div>
+    <>
+        <SEOHead
+            title={'welcome to campigns page'}
+            desc={'welcome to campigns page'}
+            // img={house?.logo}
+        />
+        <section className={`w-full h-full`}>
+            <CampaignsPage />
+        </section>
+    </>
   )
 }
 
---
+++ src/components/Campaigns/index.tsx
@@ -0,0 +1,44 @@
+import { bangers } from '@/utils/fonts'
+import { Button } from 'antd'
+import React from 'react'
+
+const CampaignsPage = () => {
+  return (
+    <section className='flex flex-col justify-center items-center px-2 mx-auto w-full overflow-x-hidden'>
+        <article>
+            <div
+                className='rounded-3xl h-[810px] w-[1428px]'
+                style={{
+                    border: '5px solid #000',
+                    backgroundImage: 'url(/assets/GIFs/campaign-gif.gif)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+            >
+            </div>
+            <div className='relative -top-[120px]'>
+                <div className={`flex justify-center items-center gap-x-[60px]`}>
+                    <Button
+                        className={`${bangers.className} h-[77px] w-[315px] flex justify-center items-center rounded-[72px] bg-yellow_primary px-10 py-3 text-5xl text-black border-none`}
+                    >
+                        Social good
+                    </Button>
+                    <Button
+                        className={`${bangers.className} h-[77px] w-[468px] flex justify-center items-center rounded-[72px] bg-sky_blue_secondary px-10 py-3 text-5xl text-black border-none`}
+                    >
+                        Dev project/grants
+                    </Button>
+                    <Button
+                        className={`${bangers.className} h-[77px] w-[265px] flex justify-center items-center rounded-[72px] bg-green_primary px-10 py-3 text-5xl text-black border-none`}
+                    >
+                        upcoming
+                    </Button>
+                </div>
+            </div>
+        </article>
+
+    </section>
+  )
+}
+
+export default CampaignsPage
