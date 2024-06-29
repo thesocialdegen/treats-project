c992226c0dbb50878c7783dd979007211dc5749f
thesocialdegen <thesocialdegen@gmail.com>
2024-06-29 11:47:48 +0530

    fix: svg changes

--- src/components/Campaigns/index.tsx
+++ src/components/Campaigns/index.tsx
@@ -178,31 +178,6 @@ const CampaignsPage = () => {
                   }}
             >
             </div>
-            {/* <div className='relative -top-[665px]'>
-                <div className={`flex justify-center items-center gap-x-[60px]`}>
-                    <h1 className='text-7xl capitalize'>support a project or smth</h1>
-                </div>
-            </div>
-            <div className='relative -top-[520px]'>
-                <div className={`flex justify-center items-center gap-x-[150px]`}>
-                    <div className='flex flex-col gap-y-6'>
-                        <p className={`${poppins.className} text-3xl w-[494px]`}>
-                            Join a supportive, collaborative ecosystem with a greater purpose – permissionless, flexible, and dedicated to defining the future of Ethereum.
-                        </p>
-                        <Button className={`${bangers.className} h-[67px] w-[365px] flex justify-center items-center text-[40px] bg-yellow_primary border-solid border-black border-[3px] rounded-[72px]`}>
-                            Sponsor a project
-                        </Button>
-                    </div>
-                    <div className='flex flex-col gap-y-4'>
-                        <p className={`${poppins.className} text-3xl w-[494px]`}>
-                            Join a supportive, collaborative ecosystem with a greater purpose – permissionless, flexible, and dedicated to defining the future of Ethereum.
-                        </p>
-                        <Button className={`${bangers.className} h-[67px] w-[345px] flex justify-center items-center text-[40px] bg-yellow_primary border-solid border-black border-[3px] rounded-[72px]`}>
-                            list your project
-                        </Button>
-                    </div>
-                </div>
-            </div> */}
         </article>
 
     </section>
