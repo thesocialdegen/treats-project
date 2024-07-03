6e03618793f54a79caf6bb5d3242b0420e7e324a
thesocialdegen <thesocialdegen@gmail.com>
2024-07-03 15:35:09 +0530

    fix: minor changes

--- src/ui-components/CustomModal/index.tsx
+++ src/ui-components/CustomModal/index.tsx
@@ -1,7 +1,7 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
+'use client';
 import { Modal } from 'antd';
 import React, { FC, useState } from 'react';
 import { useDispatch } from 'react-redux';
