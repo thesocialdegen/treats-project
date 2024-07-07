e7210e03b09b80f102f3384d4918bc6e6ba3f3ff
thesocialdegen <thesocialdegen@gmail.com>
2024-07-07 11:55:57 +0530

    fix: eslint fixes

--- src/redux/api/utils/isHydrateAction.ts
+++ src/redux/api/utils/isHydrateAction.ts
@@ -1,10 +1,10 @@
-// Copyright 2019-2025 @polka-labs/townhall authors & contributors
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { RootState } from '@/redux/@types';
 import { Action, PayloadAction } from '@reduxjs/toolkit';
 import { HYDRATE } from 'next-redux-wrapper';
-import { RootState } from '~src/redux/@types';
 
 export function isHydrateAction(action: Action): action is PayloadAction<RootState> {
 	return action.type === HYDRATE;
